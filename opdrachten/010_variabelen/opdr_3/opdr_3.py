9204fbe4c455906f8f31c3f569fc4c530a0a9149
Arnold Hilberink <104128195+NovioArnold@users.noreply.github.com>
2024-09-12 15:34:40 +0200

    opdr 10-3 klaar

--- opdrachten/010_variabelen/opdr_3/opdr_3.py
+++ opdrachten/010_variabelen/opdr_3/opdr_3.py
@@ -1,9 +1,10 @@
-# Opdracht 1
-# Naam student:
+# Opdracht 3
+# Naam student: Arnold Hilberink
 # Groep:
 
 # Hier komt je code...
 
-my_dict = ...
 
-print()
+my_dict = {'naam': 'Willem', 'achternaam': 'van der Broek', 'leeftijd': 23, 'favoriete_kleur': 'blauw'}
+
+print(my_dict)

5ab94bd819c7984a23a8c0d3c8ccdd28baac1f69
Arnold Hilberink <104128195+NovioArnold@users.noreply.github.com>
2024-09-12 15:30:11 +0200

    opdr 10-2 klaar

--- opdrachten/010_variabelen/opdr_2/opdr_2.py
+++ opdrachten/010_variabelen/opdr_2/opdr_2.py
@@ -1,7 +1,8 @@
 # Opdracht 1
-# Naam student:
-# Groep:
+# Naam student: Arnold Hilberink
+# Groep: ?
 
 # Hier komt je code...
 
-my_tuple = ...
+my_tuple = (11,'Oh en tupel', 13,5, 8009)
+print(my_tuple)

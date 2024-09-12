18dbc02d200386c446766dd595e395c35dd2ee9e
Arnold Hilberink <104128195+NovioArnold@users.noreply.github.com>
2024-09-12 16:19:38 +0200

    opdr 30-2 klaar

--- opdrachten/030_berekeningen/opdr_2/opdr_2.py
+++ opdrachten/030_berekeningen/opdr_2/opdr_2.py
@@ -4,7 +4,15 @@
 
 # Hier komt je code...
 
-c = ...
-f = ...
 
-print()
+
+def celsius_to_fahrenheit(c):
+    return 9/5 * c + 32
+
+def fahrenheit_to_celsius(f):
+    return 5/9 * (f - 32)
+
+print(celsius_to_fahrenheit(-12))
+print(fahrenheit_to_celsius(102))
+print(celsius_to_fahrenheit(62.2))
+print(fahrenheit_to_celsius(95))

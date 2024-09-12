d6661111a7123fa5ba3eda154fc41b90898c1024
Arnold Hilberink <104128195+NovioArnold@users.noreply.github.com>
2024-09-12 16:11:40 +0200

    opdr 30-1 klaar

--- opdrachten/030_berekeningen/opdr_1/opdr_1.py
+++ opdrachten/030_berekeningen/opdr_1/opdr_1.py
@@ -1,5 +1,12 @@
 # Opdracht 1 berekeningen
-# Naam student:
+# Naam student:Arnold Hilberink
 # Groep:
 
 # Hier komt je code...
+joe = 23
+bob = 15
+fred = 22
+
+print(f"Joe is {joe} jaar oud, Bob is {bob} jaar oud en Fred is {fred} jaar oud.")
+print("hun totale leeftijd is", joe + bob + fred)
+print("hun gemiddelde leeftijd is", int((joe + bob + fred) / 3))

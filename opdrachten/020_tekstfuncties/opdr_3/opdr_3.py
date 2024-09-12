1554acc64499ffa746a37e2c2d31fa689cbf7046
Arnold Hilberink <104128195+NovioArnold@users.noreply.github.com>
2024-09-12 16:07:32 +0200

    opdr 20-3 klaar

--- opdrachten/020_tekstfuncties/opdr_3/opdr_3.py
+++ opdrachten/020_tekstfuncties/opdr_3/opdr_3.py
@@ -1,5 +1,32 @@
 # Opdracht 3 tekstfuncties
-# Naam student:
+# Naam student: Arnold Hilberink
 # Groep:
 
 # Hier komt je code...
+def printboom(aantal):
+    # De template boom
+    template_boom = [
+        "    *      ",
+        "   ***     ",
+        "  ******   ",
+        " ********  ",
+        "***********",
+        "    ***    ",
+        "    ***    ",
+        "    ***    "
+    ]
+    
+    # Crieer een lijst met lege strings voor het resultaat
+    totaal_lijn = [""] * len(template_boom)
+    
+    # voor het opgegeven aantal, vermenigvuldigen van de template boom
+    for i in range(aantal):
+        for j in range(len(template_boom)):
+            totaal_lijn[j] += template_boom[j]
+    
+    # print iedere regel om net totaal aantal opgegeven bomebn te printen
+    for lijn in totaal_lijn:
+        print(lijn)
+
+# Print 5 bomen
+printboom(5)
